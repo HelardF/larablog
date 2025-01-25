2102a3a518d0ba21d759194fcfb4a6e0e6f98610
helard flores <chelard@gmail.com>
2025-01-25 17:14:13 -0500

    refactorizada las rutas, y se creo el pageController

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,14 @@
 <?php
 
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\pageController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-// Route::get Conultar
-// Route::post Guardar
-// Route::put Actualizar
-//Rote::delete eliminar
 
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/blog',function (){
-    //consulta a la base de atos
-    $posts =[
-        ['id'=>1,'title'=>'PHP','slug'=>'php'],
-        ['id'=>2,'title'=>'Javascript','slug'=>'javascript'],
-        ['id'=>3,'title'=>'Laravel','slug'=>'laravel'],
-    ];
-    return view('blog',compact('posts'));
-})->name('blog');
-
-Route::get('/blog/{slug}',function ($slug){
-    $post =$slug;
-    return view('post',['post' => $post]);
-})->name('post');
-
+Route::controller(pageController::class)->group(function () {
+    Route::get('/',             'home')->name('home');
+    Route::get('/blog',         'blog' )->name('blog');
+    Route::get('/blog/{slug}',  'post')->name('post');
+});
 
