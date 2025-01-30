1bc7c8567da1958f92b5892b421ae860ca74d17e
helard <chelard@gmail.com>
2025-01-30 18:21:50 -0500

    se instalo laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-
 use App\Http\Controllers\pageController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-
 
 Route::controller(pageController::class)->group(function () {
     Route::get('/',             'home')->name('home');
@@ -12,3 +11,17 @@ Route::controller(pageController::class)->group(function () {
     Route::get('/blog/{post:slug}',  'post')->name('post');
 });
 
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('posts', PostController::class);
+
+require __DIR__.'/auth.php';
