78ff3b3c7c857ea4bfabb0590ee5a99a0dd6c015
Helard Flores <chelard@gmail.com>
2025-01-31 15:31:53 -0500

    se ha creado el index y se le esta pasando la consulta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
 
     public function index()
     {
-        return view('posts.index');
+
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,9 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Usted esta en el index
+                    <table class="mb-4">
+                        @foreach($posts as $post)
+                            <tr class="border-b border-gray-200 text-sm">
+                                <td class="px-6 py-4"> {{$post->title}} </td>
+                                <td class="px-6 py-4">
+                                    <a href="" class="text-indigo-300">Editar</a>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="">Eliminar</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                    {{$posts->links()}}
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+
+
+
