5a65f7c5751070f6e3eaca7959ed8977f05554b0
Helard <chelard@gmail.com>
2025-02-01 15:55:39 -0500

    se agrego la validacion al formulario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,9 +27,14 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+           'title' => 'required',
+           'slug' => 'required|unique:posts,slug',
+           'body' => 'required',
+        ]);
         $post = $request->user()->posts()->create([
-           'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+           'title' => $request->title,
+            'slug' => $request->slug,
             'body' => $request->body,
 
         ]);
@@ -43,10 +48,24 @@ class PostController extends Controller
 
     }
 
-//    public function update(Request $request, Post $post)
-//    {
-//
-//    }
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts,slug,'.$post->id,
+            'body' => 'required',
+        ]);
+        $post->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body,
+
+        ]);
+        return redirect('/posts');
+
+    }
+
+
 
     public function destroy(Post $post)
     {
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,11 +1,30 @@
 @csrf
 
 <label class="uppercase text-gray-700 text-sm" >Título</label>
-<input class="rounded border-gray-200 w-full mb-4"  type="text" name="title" value="{{ $post->title }}">
+<span class="text-sm text-red-700"> @error('title') {{ $message}}@enderror</span>
+<input class="rounded border-gray-200 w-full mb-4"
+       type="text"
+       name="title"
+       value="{{ old('title', $post->title) }}">
+
+<label class="uppercase text-gray-700 text-sm" >Slug</label>
+<span class="text-sm text-red-700"> @error('slug') {{ $message}}@enderror</span>
+<input class="rounded border-gray-200 w-full mb-4"
+       type="text"
+       name="slug"
+       value="{{ old('slug', $post->slug) }}">
 
 <label class="uppercase text-gray-700 text-sm" >Contenido</label>
-<input class="rounded border-gray-200 w-full mb-4"  type="text" name="body" value="{{ $post->body }}">
+<span class="text-sm text-red-700"> @error('body') {{ $message}}@enderror</span>
+<input class="rounded border-gray-200 w-full mb-4"
+       type="text"
+       name="body"
+       value="{{ old('body', $post->body) }}">
+
 <div class="flex items-center justify-between">
     <a href="{{ route('posts.index') }}"> Volver</a>
-    <input type="submit" value="Enviar" class="text-sm bg-gray-800 text-white rouded px-2 py-1">
+    <input
+        type="submit"
+        value="Enviar"
+        class="text-sm bg-gray-800 text-white rouded px-2 py-1">
 </div>
